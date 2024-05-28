425524305fee23e0a6d71c851ba57d0667174601
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-28 10:12:40 -0300

    Adicionando imagens da estrada, ator e carro

--- sketch.js
+++ sketch.js
@@ -1,6 +1,28 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+//variáveis do carro
+let xCarro = 420;
+
+function preload() {
+    imagemDaEstrada = loadImage("assets/estrada.png");
+    imagemDoAtor = loadImage("assets/ator-1.png");
+    imagemCarro = loadImage("assets/carro-1.png");
+}
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 400);
 }
 function draw() {
-    background(220);
+    background(imagemDaEstrada);
+    mostraAtor();
+    mostraCarro();
+}
+
+function mostraAtor() {
+    image(imagemDoAtor, 100, 366, 30, 30);
+}
+
+function mostraCarro() {
+    image(imagemCarro, xCarro, 40, 50, 40);
 }
