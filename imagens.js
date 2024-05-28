c1093dfee72836d112e17ebd636a1c1342a8c107
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-28 10:43:00 -0300

    Dividindo código em arquivos

---
+++ imagens.js
@@ -0,0 +1,9 @@
+let imagemDaEstrada;
+
+let imagemCarro;
+
+function preload() {
+    imagemDaEstrada = loadImage("assets/estrada.png");
+    imagemDoAtor = loadImage("assets/ator-1.png");
+    imagemCarro = loadImage("assets/carro-1.png");
+}
